07f6ab0f4ad6db9564f2667ffb103a8e0355dff0
Sachin Jangid <sachinreal2003@gmail.com>
2025-07-27 01:54:07 +0530

    added navbar over dashboard moal

--- components/Navbar.js
+++ components/Navbar.js
@@ -46,7 +46,7 @@ export default function Navbar() {
   }, [])
 
   return (
-    <nav className="bg-white shadow-sm border-b border-gray-200 sticky top-0 z-50">
+    <nav className="bg-white shadow-sm border-b border-gray-200 sticky top-0 z-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Left - Brand */}
