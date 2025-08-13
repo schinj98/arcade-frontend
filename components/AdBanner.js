7c68f9d2aa0ce3b5d12a6dc2b147974e49432e83
Sachin Jangid <sachinreal2003@gmail.com>
2025-08-13 16:50:25 +0530

    added ads for desktop in incompletelabs21

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -11,15 +11,17 @@ export default function AdBanner() {
   }, []);
 
   return (
-    <div className="my-4">
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-5183171666938196"
-        data-ad-slot="3736968322"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
+    <div className="my-4 flex justify-center">
+      <div className="w-full h-auto min-h-[250px]">
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block", width: "100%", height: "100%" }}
+          data-ad-client="ca-pub-5183171666938196"
+          data-ad-slot="3736968322"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </div>
     </div>
   );
 }
